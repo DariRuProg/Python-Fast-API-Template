4b0e0b4ca6a984ed21845cfaa067870bf23aa776
DariRuProg <dar.onl.work@gmail.com>
2024-03-20 22:36:13 +0100

    Update tools.py

--- app/routers/tools.py
+++ app/routers/tools.py
@@ -23,45 +23,17 @@ from ..services import llm_api as llm, prompts as pr
 logger = logging.getLogger("AppLogger")
 router = APIRouter()
 
+@router.get("/seo/google_keyw_rank")
+async def root():
+    return "healthy"
+
+
+"""
 def extract_h_titles(url: str) -> List[str]:
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
     htitles = [h.text for h in soup.find_all(["h1", "h2", "h3", "h4", "h5", "h6"])]
     return htitles
-
-
-""" @router.get("/seo/google_keyw_rank")
-async def root(keyword: str, num_results: int = 10, n_pages: int = 1):
-    results = []
-    counter = 0
-    for page in range(0, n_pages):
-        url = f"https://www.google.com/search?q={keyword}&start={page * 10}"
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-        }
-        response = requests.get(url, headers=headers)
-        soup = BeautifulSoup(response.content, "html.parser")
-        search = soup.find_all("div", class_="tF2Cxc")
-
-        for h in search:
-            if counter == num_results:
-                break
-            counter += 1
-            title = h.find("h3").text
-            link = h.find("a")["href"]
-            rank = counter
-            htitles = extract_h_titles(link)
-
-            results.append(
-                {
-                    "title": title,
-                    "url": link,
-                    "rank": rank,
-                    "htitles": htitles,
-                }
-            )
-
-    return {"results": results} """
 
 
 @router.get("/seo/google_keyw_rank")
@@ -101,4 +73,4 @@ async def root(keyword: str, num_results: int = 10, n_pages: int = 1):
         else:
             return {"success": True, "message": "Generated Titles Successfully", "result": results}
     except Exception as e:
-        return {"success": False, "message": f"An error occurred: {str(e)}"}
+        return {"success": False, "message": f"An error occurred: {str(e)}"} """
